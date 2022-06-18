b2ffd33e34c8deaeb388426c596f19b27c92c31c
Алексей Комлев <komlev.2236@gmail.com>
2022-06-18 23:09:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 const numbers = document.querySelector('#input__numbers'),
-    loverCase = document.querySelector('#input__loverase'),
-    upperCase = document.querySelector('#input__uppercase'),
+    lowercase = document.querySelector('#input__lowercase'),
+    uppercase = document.querySelector('#input__uppercase'),
     symbol = document.querySelector('#input__symbol'),
     lengthPassword = document.querySelector('.input__length'),
     button = document.querySelector('.btn__password');
@@ -11,65 +11,88 @@ let p1 = '',
     p3 = "",
     p4 = "",
     p5 = "";
-
 let randomIndex = 0;
+
+let result = [];
 
 let numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-let loverCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let upperCaseArray = [];
-loverCaseArray.forEach((item) => {
-    upperCaseArray.push(item.toUpperCase());
-});
+let lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z', 'J', 'U', 'W'];
+
 
 let symbolArray = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', '.', '?', '/'];
 
-function generatorPassword() {
+
+
+function generatorPassword(arr) {
     for (let i = 0; i < lengthPassword.value; i++) {
 
-        randomIndex = Math.floor(Math.random() * loverCaseArray.length);
-        p1 += loverCaseArray[randomIndex];
+        randomIndex = Math.floor(Math.random() * arr.length);
+        p1 += arr[randomIndex];
 
-        randomIndex = Math.floor(Math.random() * loverCaseArray.length);
-        p2 += loverCaseArray[randomIndex];
+        randomIndex = Math.floor(Math.random() * arr.length);
+        p2 += arr[randomIndex];
 
-        randomIndex = Math.floor(Math.random() * loverCaseArray.length);
-        p3 += loverCaseArray[randomIndex];
+        randomIndex = Math.floor(Math.random() * arr.length);
+        p3 += arr[randomIndex];
 
-        randomIndex = Math.floor(Math.random() * loverCaseArray.length);
-        p4 += loverCaseArray[randomIndex];
+        randomIndex = Math.floor(Math.random() * arr.length);
+        p4 += arr[randomIndex];
 
-        randomIndex = Math.floor(Math.random() * loverCaseArray.length);
-        p5 += loverCaseArray[randomIndex];
+        randomIndex = Math.floor(Math.random() * arr.length);
+        p5 += arr[randomIndex];
     }
+
+
 }
 
 function showPassword() {
-    document.querySelector('.wrapper__result').innerHTML = ` <hr>
-    <div class="option__password">
-        <h3 class="subtitle">Выберите выриант: </h3>
-        <p class="password">${p1}</p>
-        <p class="password">${p2}</p>
-        <p class="password">${p3}</p>
-        <p class="password">${p4}</p>
-        <p class="password">${p5}</p>
-    </div>`;
+    let display = '';
+    if (result.length == 0) {
+        display = `<h2 class="subtitle">Заполните форму</h2>`;
+    } else {
+        display = ` <hr>
+        <div class="option__password">
+            <h3 class="subtitle">Выберите выриант: </h3>
+            <p class="password">${p1}</p>
+            <p class="password">${p2}</p>
+            <p class="password">${p3}</p>
+            <p class="password">${p4}</p>
+            <p class="password">${p5}</p>
+        </div>`;
+    }
+    document.querySelector('.wrapper__result').innerHTML = display;
 }
+
 button.addEventListener('click', () => {
     if (numbers.querySelector('input:checked')) {
-        loverCaseArray.push(...numberArray);
+        result.push(...numberArray);
     }
-    if (upperCase.querySelector('input:checked')) {
-        loverCaseArray.push(...upperCaseArray);
+
+    if (lowercase.querySelector('input:checked')) {
+        result.push(...lowercaseArray);
     }
+
+    if (uppercase.querySelector('input:checked')) {
+        result.push(...upperCaseArray);
+    }
+
     if (symbol.querySelector('input:checked')) {
-        loverCaseArray.push(...symbolArray);
+        result.push(...symbolArray);
     }
-    generatorPassword();
+
+    if (numbers.querySelector('input:checked') && uppercase.querySelector('input:checked') && lowercase.querySelector('input:checked') && symbol.querySelector('input:checked')) {
+        result.push(...numberArray, ...upperCaseArray, ...lowercaseArray, ...symbolArray);
+    }
+
+
+    generatorPassword(result);
     showPassword();
     p1 = '';
     p2 = '';
     p3 = '';
     p4 = '';
     p5 = '';
+    result.splice(0, result.length);
 });
